29a2f07e478a352f04e01c6f6e38b1b20afab815
M4ldu <84003143+M4ldu@users.noreply.github.com>
2021-05-11 07:56:57 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,10 @@
+let Zähler = 0
+input.onButtonPressed(Button.A, function () {
+    Zähler += 0
+    for (let index = 0; index < 10; index++) {
+        Zähler += 1
+    }
+})
+basic.forever(function () {
+	
+})
